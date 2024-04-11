e9092ac665789d38f7b12f7936e8100d4837184e
julien44830 <deniaudjuli0@gmail.com>
2024-04-11 16:52:45 +0200

    ✨✨ finalisation de la terre 3D et de son css && importation des polices dans le fichier app.css ✨✨

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,43 +7,34 @@ import './styles/header_home.css'
 function Header() {
 
     useEffect(() => {
-
         const scene = new THREE.Scene();
-        const ambientLight = new THREE.AmbientLight(0xffffff, 7); // effet de lumière sur la terre
-        const camera = new THREE.PerspectiveCamera(5, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const ambientLight = new THREE.AmbientLight(0xffffff, 10); // effet de lumière sur la terre
+        const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const axis = new THREE.Vector3(1, 1, 1); // Par exemple, l'axe des y
         const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('3d-canvas') });
-        scene.background = new THREE.Color("rgba(255, 255, 255, 1)"); 
-        scene.add(ambientLight);
-        renderer.setSize(window.innerWidth, window.innerHeight);
-
         const loader = new GLTFLoader();
+        scene.background = new THREE.Color("rgba(255, 255, 255, 1)"); 
+        scene.add( ambientLight, axis );
+        renderer.setSize(window.innerWidth, window.innerHeight);
         
         loader.load(
             '../src/assets/images/earth_3D.glb', // fichier_3D
             (glb) => {
                 const earth = glb.scene;
                 scene.add(earth);
-
                 function animate(){
                     requestAnimationFrame(animate);
                     earth.rotation.y += 0.002; // Vitesse de rotation
                     renderer.render(scene, camera);
                 }
-
                 animate(); 
             },
-
-            // (xhr) => {
-            //     console.warn((xhr.loaded / xhr.total) * 100 + '% chargé');
-            // },
-
             (err) => {
                 console.error('Erreur de chargement du modèle', err);
             }
         );
-
-        camera.position.z = 0.9 ; // Augmentez pour zoomer
-        camera.fov = 30; // Diminuez pour dezoomer
+        camera.position.z = 90; // Augmentez pour zoomer
+        camera.fov = 0.26; // Diminuez pour dezoomer
         camera.updateProjectionMatrix();
     }, []); 
 
@@ -54,8 +45,7 @@ function Header() {
                 <canvas id="3d-canvas" />
             </div>
         </header>
-
     );
 }
-
+ 
 export default Header;
