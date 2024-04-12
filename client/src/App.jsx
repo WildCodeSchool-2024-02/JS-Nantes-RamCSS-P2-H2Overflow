63424e6dae1b44684cf9c8af74bee85f0d794804
Lucasa49 <lucasaksu@gmail.com>
2024-04-12 14:14:50 +0200

    insertion_text_pageloader1/

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import TextPageLoader from "./components/TextPageLoader";
 
 function App() {
   return (
-    
-    <Header/>
-    
+    <>
+      <Header />
+      <TextPageLoader />
+    </>
+
   );
 }
 
---
+++ client/src/components/TextPageLoader.jsx
@@ -0,0 +1,12 @@
+function TextPageLoader() {
+
+    return (
+        <>
+            <p className="PLoader">Découvrez les profondeurs invisibles
+                Explorez  les nappes phréatiques pour comprendre leur importance et leur préservation.</p>
+        <button className="ButtonLoader" type="button" ><a href=""></a> A </button>
+       
+      </>
+    )
+}
+export default TextPageLoader
