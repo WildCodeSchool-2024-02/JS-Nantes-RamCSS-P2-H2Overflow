11a47d872657d3a4d33fc1630a590e18b4c0e886
Pas-nou <pascal.renou@live.fr>
2024-04-12 12:18:44 +0200

    US_P2_04 : � creation of a component to visualize the water level

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import Groundwatercard from "./components/Groundwatercard"
+
 
 function App() {
   return (
-    
-    <Header/>
-    
+    <>
+      <Header />
+    </>
   );
 }
 
