be57b73f6702be7c92259c5497f5a55df4836630
Lucasa49 <lucasaksu@gmail.com>
2024-04-16 17:14:42 +0200

    mention légal debut

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import PagePrincipale from "./pages/PagePrincipale";
+import MentionLégales from "./pages/mentionslégales";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
       {
         path: "/PagePrincipale",
         element: <PagePrincipale />,
+      },
+      {
+        path: "/mentionlégales",
+        element: <MentionLégales />,
       },
     ],
   },
---
+++ client/src/pages/mentionslégales.jsx
@@ -0,0 +1,32 @@
+function MentionLégales() {
+  return (
+    <>
+      <h1 className="mentionlégaltitre">Mention légales</h1>
+      <p className="mentionlégalp">
+        Ce site web est édité par [Nom de la société], une société [forme
+        juridique] au capital de [montant du capital social], immatriculée au
+        Registre du Commerce et des Sociétés de [ville] sous le numéro [numéro
+        RCS], dont le siège social est situé au [adresse complète]. Directeur de
+        la publication : [Nom du directeur de la publication] Contact : [Adresse
+        email de contact] Hébergement : Ce site est hébergé par [Nom de
+        l'hébergeur], dont le siège social est situé au [adresse complète].
+        Propriété Intellectuelle : Tous les éléments présents sur ce site, y
+        compris mais non limité aux textes, images, logos, graphiques, vidéos,
+        et autres contenus, sont la propriété intellectuelle de [Nom de la
+        société] ou de ses concédants de licence, et sont protégés par les lois
+        françaises et internationales sur le droit d'auteur et la propriété
+        intellectuelle. Toute reproduction, distribution, modification ou
+        utilisation de ces éléments sans l'autorisation préalable écrite de [Nom
+        de la société] est strictement interdite. Collecte de données
+        personnelles : [Nom de la société] collecte et traite les données
+        personnelles conformément à sa politique de confidentialité, disponible
+        à l'adresse suivante : [lien vers la politique de confidentialité].
+        Cookies : Ce site utilise des cookies. Pour plus d'informations sur
+        l'utilisation des cookies et sur la manière de les gérer ,veuillez
+        consulter notre politique en matière de cookies disponible à l'adresse
+        suivante : [lien vers la politique en matière de
+      </p>
+    </>
+  );
+}
+export default MentionLégales;
