6c3803b0da104ee0f23ba0eecb809232d0e0e8d9
julien44830 <deniaudjuli0@gmail.com>
2024-04-16 16:03:15 +0200

    ajout de navlink

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { useLocation } from "react-router-dom"; // Importez useLocation depuis react-router-dom
+import { NavLink, useLocation } from "react-router-dom";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"; // Import correct
@@ -7,7 +7,7 @@ import "./styles/header.css";
 
 function Header() {
   const [isLoaded, setIsLoaded] = useState(false);
-  const location = useLocation()
+  const location = useLocation();
 
   const canvas = useRef();
   const camera = useRef();
@@ -61,14 +61,16 @@ function Header() {
     <header
       className={`header ${location.pathname === "/" ? "header-home" : ""}`}
     >
-      <h1 className={`header ${location.pathname === "/" ? "h1-home":""}`} >H²OVERFLOW</h1>
-      <div>
+      <h1 className={`header ${location.pathname === "/" ? "h1-home" : ""}`}>
+        H²OVERFLOW
+      </h1>
+      <NavLink to="/">
         <canvas
           ref={canvas}
           id="3d-canvas"
           className={`header ${location.pathname === "/" ? "canvas-animate-home canvas-home earth-3D-home" : ""} earth-3D ${isLoaded ? "canvas-animate" : ""}`}
         />
-      </div>
+      </NavLink>
     </header>
   );
 }
