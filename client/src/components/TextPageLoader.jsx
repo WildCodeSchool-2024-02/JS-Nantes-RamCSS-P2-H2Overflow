c082a07a3f88a014a642fef4f96472c3e708ab2c
julien44830 <deniaudjulien0@gmail.com>
2024-04-17 00:43:00 +0200

    mise en place de fetch dans router pour page home✅

--- client/src/components/TextPageLoader.jsx
+++ client/src/components/TextPageLoader.jsx
@@ -1,13 +1,20 @@
 import { NavLink } from "react-router-dom";
 
-function TextPageLoader() {
+function TextPageLoader(data) {
+  const { data: dataContent } = data;
+
   return (
     <section className="section_loader">
       <h2 className="h2-loader">Découvrez les profondeurs invisibles</h2>
 
       <p className="p-loader">
-        Explorez les nappes phréatiques pour comprendre <br /> leur importance
-        et leur préservation.
+        Explorez les nappes phréatiques pour comprendre
+        <br />
+        leur importance et leur préservation.
+        <br />
+        <br />
+        Notre base de donnée est composée de {dataContent} tableaux de source
+        diférentes.
       </p>
 
       <NavLink className="boutton" to="/map">
