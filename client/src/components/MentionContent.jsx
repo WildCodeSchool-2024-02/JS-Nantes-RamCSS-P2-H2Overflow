ec911f94bc08c0666382ea05d1df23206328df6f
Lucasa49 <lucasaksu@gmail.com>
2024-04-24 11:20:05 +0200

    legal mention fixed bug...

--- client/src/components/MentionContent.jsx
+++ client/src/components/MentionContent.jsx
@@ -5,28 +5,26 @@ function MentionContent() {
 
       <p className="mentionlegalp">
         {" "}
-        <br /> Ce site web est édité par [Nom de la société], une société [forme
+        Ce site web est édité par [Nom de la société], une société [forme
         juridique] au capital de [montant du capital social],
       </p>
 
       <p className="mentionlegalp2">
         {" "}
-        <br />
         immatriculée au Registre du Commerce et des Sociétés de [ville] sous le
         numéro [numéro RCS], dont le siège social est situé au [adresse ].
       </p>
 
       <p className="mentionlegalp3">
         {" "}
-        <br /> Directeur de la publication : [Nom du directeur de la
-        publication] Contact : [Adresse email de contact]
-        <br /> Hébergement : Ce site est hébergé par [Nom de l'hébergeur], dont
-        le siège social est situé au [adresse complète].
+        Directeur de la publication : [Nom du directeur de la publication]
+        Contact : [Adresse email de contact] Hébergement : Ce site est hébergé
+        par [Nom de l'hébergeur], dont le siège social est situé au [adresse
+        complète].
       </p>
 
       <p className="mentionlegalp4">
         {" "}
-        <br />
         Propriété Intellectuelle : Tous les éléments présents sur ce site, y
         compris mais non limité aux textes, images, logos, graphiques, vidéos,
         et autres contenus, sont la propriété intellectuelle de [Nom de la
@@ -37,7 +35,6 @@ function MentionContent() {
 
       <p className="mentionlegalp5">
         {" "}
-        <br />
         Toute reproduction, distribution, modification ou utilisation de ces
         éléments sans l'autorisation préalable écrite de [Nom de la société] est
         strictement interdite.
@@ -45,7 +42,6 @@ function MentionContent() {
 
       <p className="mentionlegalp6">
         {" "}
-        <br />
         Collecte de données personnelles : [Nom de la société] collecte et
         traite les données personnelles conformément à sa politique de
         confidentialité, disponible à l'adresse suivante : [lien vers la
@@ -54,8 +50,112 @@ function MentionContent() {
 
       <p className="mentionlegalp7">
         {" "}
-        <br /> Cookies : Ce site utilise des cookies. Pour plus d'informations
-        sur l'utilisation des cookies et sur la manière de les gérer, veuillez
+        Cookies : Ce site utilise des cookies. Pour plus d'informations sur
+        l'utilisation des cookies et sur la manière de les gérer, veuillez
+        consulter notre politique en matière de cookies disponible à l'adresse
+        suivante : [lien vers la politique en matière de cookies].
+      </p>
+      <p className="mentionlegalp">
+        {" "}
+        Ce site web est édité par [Nom de la société], une société [forme
+        juridique] au capital de [montant du capital social],
+      </p>
+
+      <p className="mentionlegalp2">
+        {" "}
+        immatriculée au Registre du Commerce et des Sociétés de [ville] sous le
+        numéro [numéro RCS], dont le siège social est situé au [adresse ].
+      </p>
+
+      <p className="mentionlegalp3">
+        {" "}
+        Directeur de la publication : [Nom du directeur de la publication]
+        Contact : [Adresse email de contact] Hébergement : Ce site est hébergé
+        par [Nom de l'hébergeur], dont le siège social est situé au [adresse
+        complète].
+      </p>
+
+      <p className="mentionlegalp4">
+        {" "}
+        Propriété Intellectuelle : Tous les éléments présents sur ce site, y
+        compris mais non limité aux textes, images, logos, graphiques, vidéos,
+        et autres contenus, sont la propriété intellectuelle de [Nom de la
+        société] ou de ses concédants de licence, et sont protégés par les lois
+        françaises et internationales sur le droit d'auteur et la propriété
+        Intellectuelle.
+      </p>
+
+      <p className="mentionlegalp5">
+        {" "}
+        Toute reproduction, distribution, modification ou utilisation de ces
+        éléments sans l'autorisation préalable écrite de [Nom de la société] est
+        strictement interdite.
+      </p>
+
+      <p className="mentionlegalp6">
+        {" "}
+        Collecte de données personnelles : [Nom de la société] collecte et
+        traite les données personnelles conformément à sa politique de
+        confidentialité, disponible à l'adresse suivante : [lien vers la
+        politique de confidentialité].
+      </p>
+
+      <p className="mentionlegalp7">
+        {" "}
+        Cookies : Ce site utilise des cookies. Pour plus d'informations sur
+        l'utilisation des cookies et sur la manière de les gérer, veuillez
+        consulter notre politique en matière de cookies disponible à l'adresse
+        suivante : [lien vers la politique en matière de cookies].
+      </p>
+      <p className="mentionlegalp">
+        {" "}
+        Ce site web est édité par [Nom de la société], une société [forme
+        juridique] au capital de [montant du capital social],
+      </p>
+
+      <p className="mentionlegalp2">
+        {" "}
+        immatriculée au Registre du Commerce et des Sociétés de [ville] sous le
+        numéro [numéro RCS], dont le siège social est situé au [adresse ].
+      </p>
+
+      <p className="mentionlegalp3">
+        {" "}
+        Directeur de la publication : [Nom du directeur de la publication]
+        Contact : [Adresse email de contact] Hébergement : Ce site est hébergé
+        par [Nom de l'hébergeur], dont le siège social est situé au [adresse
+        complète].
+      </p>
+
+      <p className="mentionlegalp4">
+        {" "}
+        Propriété Intellectuelle : Tous les éléments présents sur ce site, y
+        compris mais non limité aux textes, images, logos, graphiques, vidéos,
+        et autres contenus, sont la propriété intellectuelle de [Nom de la
+        société] ou de ses concédants de licence, et sont protégés par les lois
+        françaises et internationales sur le droit d'auteur et la propriété
+        Intellectuelle.
+      </p>
+
+      <p className="mentionlegalp5">
+        {" "}
+        Toute reproduction, distribution, modification ou utilisation de ces
+        éléments sans l'autorisation préalable écrite de [Nom de la société] est
+        strictement interdite.
+      </p>
+
+      <p className="mentionlegalp6">
+        {" "}
+        Collecte de données personnelles : [Nom de la société] collecte et
+        traite les données personnelles conformément à sa politique de
+        confidentialité, disponible à l'adresse suivante : [lien vers la
+        politique de confidentialité].
+      </p>
+
+      <p className="mentionlegalp7">
+        {" "}
+        Cookies : Ce site utilise des cookies. Pour plus d'informations sur
+        l'utilisation des cookies et sur la manière de les gérer, veuillez
         consulter notre politique en matière de cookies disponible à l'adresse
         suivante : [lien vers la politique en matière de cookies].
       </p>
