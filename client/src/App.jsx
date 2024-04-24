9b2d28bbec54ef1654e735d14f2c3cee646d1ef9
julien44830 <deniaudjuli0@gmail.com>
2024-04-24 15:24:50 +0200

    correction du fetch App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,19 +9,34 @@ function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch(
-      "https://hubeau.eaufrance.fr/api//v1/niveaux_nappes/chroniques_tr?page=20&size=1000"
-    )
+    fetch("https://hubeau.eaufrance.fr/api/v1/niveaux_nappes/stations")
       .then((response) => response.json())
       .then((resData) => {
         setData(resData);
         setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(
+          "Une erreur s'est produite lors de la récupération des données:",
+          error
+        );
+        setIsLoading(false); // Assurez-vous que isLoading est toujours réinitialisé, même en cas d'erreur
       });
   }, []);
 
-  return isLoading ? (
-    <Spinner />
-  ) : (
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  const yearsArray = [];
+  for (let i = 0; i < data.data.length; i += 1) {
+    const datesArray = data.data[i].profondeur_investigation;
+    const yearsString = datesArray;
+    yearsArray.push(yearsString);
+  }
+  // const years = [...new Set(yearsArray)];
+
+  return (
     <>
       <Header data={data} />
       <Outlet />
